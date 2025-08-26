a3fa4688d23d8b67bb22f8bc745b683c112f8051
isabuleje <ibdb.snf24@uea.edu.br>
2025-08-25 20:53:06 -0400

    login page
    tô só reutilizando oq já tinha feito na pizzaria, só pra ter algo logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import TaskList from './components/TaskList';
 import PomodoroTimer from './components/PomodoroTimer';
 import Header from './components/Header'
 
+
 function App() {
   return (
     <main className="dashboard">
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, TextField, Button, Typography, Paper, Container, Icon } from '@mui/material';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+
+const Login = () =>{
+    return(
+    <Box component="section" maxWidth="xs" sx={{ 
+      backgroundImage: 'url(/images/madeira.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+     }}>
+      <Container component="main" maxWidth="xs" sx={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',  
+      }}>
+
+        <Paper elevation={3} sx={{ padding: 4, width: '100%'}}>
+          <Button
+            variant="outlined"
+            startIcon={<KeyboardReturnIcon />}
+            onClick={() => navigate('/')}
+          >
+            Retornar 
+          </Button>
+          <Typography component="h1" variant="h5" align="center" marginTop={5}>
+            Login
+          </Typography>
+          <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 1, mb: 3 }}>
+            Faça login para acessar o sistema
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Usuário"
+              name="username"
+              autoComplete="username"
+              autoFocus
+
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              color='primary'
+            >
+              Entrar
+            </Button>
+          </Box>
+        </Paper>
+        </Container>
+    </Box>
+    );
+}
+
+export default Login;
